4d1d5c81c469bad84b92e2519ecf15be7e594c84
easydong02 <easydong02@gmail.com>
2022-11-18 17:11:55 +0900

    add markers in MapView

--- front/src/components/map/MapView.vue
+++ front/src/components/map/MapView.vue
@@ -7,16 +7,47 @@
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
 import DealView from "@/components/map/DealView.vue";
 import SearchView from "@/components/map/SearchView.vue";
+import { apiInstance } from "@/api/index.js";
+
+const storeName = "dealViewStore";
+const http = apiInstance();
+
 export default {
   components: { DealView, SearchView },
   name: "MapView",
+
+  data() {
+    return {
+      markers: [],
+      map: "",
+      curIndex: -1,
+      dealInfo: [],
+    };
+  },
 
   component: {
     DealView,
     SearchView,
   },
+  computed: {
+    ...mapState(storeName, ["gu", "dong", "houseList", "fromMainKeyword"]),
+  },
+  watch: {
+    houseList: function () {
+      console.log("houseLIst change");
+      this.removeMarkers();
+      if (this.houseList.length) {
+        this.addMarkers(this.houseList);
+        console.log(this.houseList);
+      } else {
+        alert("정보가 없습니다.");
+      }
+    },
+  },
+
   mounted() {
     // kakao map 초기화
     if (window.kakao && window.kakao.maps) {
@@ -36,6 +67,7 @@ export default {
   },
 
   methods: {
+    ...mapActions(storeName, ["getHouseDeal"]),
     initMap() {
       console.log("initMap");
       const mapContainer = document.getElementById("map");
@@ -47,6 +79,62 @@ export default {
       };
       console.log(mapOption);
       this.map = new kakao.maps.Map(mapContainer, mapOption);
+    },
+
+    //지도 관련 메소드
+    removeMarkers() {
+      // if (this.curInfoWindow) {
+      //   this.curInfoWindow.close();
+      //   this.curInfoWindow = null;
+      // }
+
+      // this.infoWindows.forEach(i => i.close());
+      // this.infoWindows = [];
+      this.markers.forEach((m) => m.setMap(null));
+      this.markers = [];
+    },
+    addMarkers(list) {
+      let bounds = new kakao.maps.LatLngBounds();
+
+      list.forEach(({ lat, lng }, index) => {
+        // console.log(`forEach ${index}`);
+        let markerPosition = new kakao.maps.LatLng(lat, lng);
+        this.addMarkerByOne(markerPosition, index);
+        bounds.extend(markerPosition);
+      });
+      // this.addInfoWindow();
+      this.map.setBounds(bounds);
+    },
+    addMarkerByOne(markerPosition, index) {
+      let marker = new kakao.maps.Marker({
+        position: markerPosition,
+      });
+      // let $this = this;
+      kakao.maps.event.addListener(marker, "click", function () {
+        // $this.showHouseDetail(index);
+        alert("인덱스:" + index);
+      });
+      this.markers.push(marker);
+      marker.setMap(this.map);
+    },
+    showHouseDetail(index) {
+      this.curIndex = index;
+      const houseNo = this.houseList[index].houseNo;
+      this.getHouseDeal(houseNo);
+      this.getOngoingList(houseNo);
+      this.getHouseReview(houseNo);
+      // if (!this.listVisible) this.listVisible = true;
+    },
+    getHouseDeal(houseNo) {
+      http
+        .get(`/house/deal/${houseNo}`)
+        .then((response) => {
+          this.dealInfo = response.data.houseDealDto;
+        })
+        .catch((error) => {
+          // this.$swal("서버에 문제가 발생하였습니다.", { icon: "error" });
+          alert("하나의 정보를 가져오는데 오류 발생:" + error);
+        });
     },
   },
 };
