8d857b6ed9c198dba1bc0d485f26a8dc93e1a8a1
dyeon7310 <dyeon7310@gmail.com>
2022-11-23 17:33:27 +0900

    [1123 doyeon] 로그인 폼 수정 및 이미지 추가

--- front/src/components/user/UserLogin.vue
+++ front/src/components/user/UserLogin.vue
@@ -1,6 +1,29 @@
 <template>
-  <b-container class="bv-example-row mt-3">
-    <b-row>
+  <b-container class="mt-3">
+    <div class="container">
+      <div class="form-structor m-auto">
+        <div class="signup">
+          <h2 class="form-title" id="signup"><span>or</span>Sign up</h2>
+          <div class="form-holder">
+            <input type="text" class="input" placeholder="Name" />
+            <input type="email" class="input" placeholder="Email" />
+            <input type="password" class="input" placeholder="Password" />
+          </div>
+          <button class="submit-btn">Sign up</button>
+        </div>
+        <div class="login slide-up">
+          <div class="center">
+            <h2 class="form-title" id="login"><span>or</span>Log in</h2>
+            <div class="form-holder">
+              <input type="email" class="input" placeholder="Email" />
+              <input type="password" class="input" placeholder="Password" />
+            </div>
+            <button class="submit-btn">Log in</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <b-row>
       <b-col>
         <b-alert variant="secondary" show><h3>로그인</h3></b-alert>
       </b-col>
@@ -50,7 +73,7 @@
         </b-card>
       </b-col>
       <b-col></b-col>
-    </b-row>
+    </b-row> -->
   </b-container>
 </template>
 
@@ -92,4 +115,275 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Fira+Sans");
+
+// html,
+// body {
+//   position: relative;
+//   min-height: 100vh;
+//   background-color: #e1e8ee;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   font-family: "Fira Sans", Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+// }
+
+.form-structor {
+  background-color: #222;
+  border-radius: 15px;
+  height: 700px;
+  width: 500px;
+  position: relative;
+  overflow: hidden;
+
+  &::after {
+    content: "";
+    opacity: 0.8;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-repeat: no-repeat;
+    background-position: left bottom;
+    background-size: 500px;
+    background-image: url("../../assets/images/loginimage.jpg");
+  }
+
+  .signup {
+    position: absolute;
+    top: 35%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    width: 65%;
+    z-index: 5;
+    -webkit-transition: all 0.3s ease;
+
+    &.slide-up {
+      top: 5%;
+      -webkit-transform: translate(-50%, 0%);
+      -webkit-transition: all 0.3s ease;
+    }
+
+    &.slide-up .form-holder,
+    &.slide-up .submit-btn {
+      opacity: 0;
+      visibility: hidden;
+    }
+
+    &.slide-up .form-title {
+      font-size: 1em;
+      cursor: pointer;
+    }
+
+    &.slide-up .form-title span {
+      margin-right: 5px;
+      opacity: 1;
+      visibility: visible;
+      -webkit-transition: all 0.3s ease;
+    }
+
+    .form-title {
+      color: #fff;
+      font-size: 1.7em;
+      text-align: center;
+
+      span {
+        color: rgba(0, 0, 0, 0.4);
+        opacity: 0;
+        visibility: hidden;
+        -webkit-transition: all 0.3s ease;
+      }
+    }
+
+    .form-holder {
+      border-radius: 15px;
+      background-color: #fff;
+      overflow: hidden;
+      margin-top: 30px;
+      opacity: 1;
+      visibility: visible;
+      -webkit-transition: all 0.3s ease;
+
+      .input {
+        border: 0;
+        outline: none;
+        box-shadow: none;
+        display: block;
+        height: 50px;
+        line-height: 50px;
+        padding: 8px 15px;
+        border-bottom: 1px solid #eee;
+        width: 100%;
+        font-size: 15px;
+
+        &:last-child {
+          border-bottom: 0;
+        }
+        &::-webkit-input-placeholder {
+          color: rgba(0, 0, 0, 0.4);
+        }
+      }
+    }
+
+    .submit-btn {
+      background-color: rgba(0, 0, 0, 0.4);
+      color: rgba(256, 256, 256, 0.7);
+      border: 0;
+      border-radius: 15px;
+      display: block;
+      margin: 15px auto;
+      padding: 15px 45px;
+      width: 100%;
+      font-size: 13px;
+      font-weight: bold;
+      cursor: pointer;
+      opacity: 1;
+      visibility: visible;
+      -webkit-transition: all 0.3s ease;
+
+      &:hover {
+        transition: all 0.3s ease;
+        background-color: rgba(0, 0, 0, 0.8);
+      }
+    }
+  }
+
+  .login {
+    position: absolute;
+    top: 20%;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #fff;
+    z-index: 5;
+    -webkit-transition: all 0.3s ease;
+
+    &::before {
+      content: "";
+      position: absolute;
+      left: 50%;
+      top: -20px;
+      -webkit-transform: translate(-50%, 0);
+      background-color: #fff;
+      width: 200%;
+      height: 250px;
+      border-radius: 50%;
+      z-index: 4;
+      -webkit-transition: all 0.3s ease;
+    }
+
+    .center {
+      position: absolute;
+      top: calc(50% - 10%);
+      left: 50%;
+      -webkit-transform: translate(-50%, -50%);
+      width: 65%;
+      z-index: 5;
+      -webkit-transition: all 0.3s ease;
+
+      .form-title {
+        color: #000;
+        font-size: 1.7em;
+        text-align: center;
+
+        span {
+          color: rgba(0, 0, 0, 0.4);
+          opacity: 0;
+          visibility: hidden;
+          -webkit-transition: all 0.3s ease;
+        }
+      }
+
+      .form-holder {
+        border-radius: 15px;
+        background-color: #fff;
+        border: 1px solid #eee;
+        overflow: hidden;
+        margin-top: 50px;
+        opacity: 1;
+        visibility: visible;
+        -webkit-transition: all 0.3s ease;
+
+        .input {
+          border: 0;
+          outline: none;
+          box-shadow: none;
+          display: block;
+          height: 30px;
+          line-height: 30px;
+          padding: 8px 15px;
+          border-bottom: 1px solid #eee;
+          width: 100%;
+          font-size: 12px;
+
+          &:last-child {
+            border-bottom: 0;
+          }
+          &::-webkit-input-placeholder {
+            color: rgba(0, 0, 0, 0.4);
+          }
+        }
+      }
+
+      .submit-btn {
+        background-color: #6b92a4;
+        color: rgba(256, 256, 256, 0.7);
+        border: 0;
+        border-radius: 15px;
+        display: block;
+        margin: 15px auto;
+        padding: 15px 45px;
+        width: 100%;
+        font-size: 13px;
+        font-weight: bold;
+        cursor: pointer;
+        opacity: 1;
+        visibility: visible;
+        -webkit-transition: all 0.3s ease;
+
+        &:hover {
+          transition: all 0.3s ease;
+          background-color: rgba(0, 0, 0, 0.8);
+        }
+      }
+    }
+
+    &.slide-up {
+      top: 90%;
+      -webkit-transition: all 0.3s ease;
+    }
+
+    &.slide-up .center {
+      top: 10%;
+      -webkit-transform: translate(-50%, 0%);
+      -webkit-transition: all 0.3s ease;
+    }
+
+    &.slide-up .form-holder,
+    &.slide-up .submit-btn {
+      opacity: 0;
+      visibility: hidden;
+      -webkit-transition: all 0.3s ease;
+    }
+
+    &.slide-up .form-title {
+      font-size: 1em;
+      margin: 0;
+      padding: 0;
+      cursor: pointer;
+      -webkit-transition: all 0.3s ease;
+    }
+
+    &.slide-up .form-title span {
+      margin-right: 5px;
+      opacity: 1;
+      visibility: visible;
+      -webkit-transition: all 0.3s ease;
+    }
+  }
+}
+</style>
--- front/src/views/UserView.vue
+++ front/src/views/UserView.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container class="bv-example-row mt-3 text-center">
     <h3 class="underline-steelblue">
-      <b-icon icon="person-lines-fill"></b-icon>
+      <b-icon icon="person-circle"></b-icon>
     </h3>
     <router-view></router-view>
   </b-container>
